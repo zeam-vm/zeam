ff69c15612bc7ab559fdff9f7e2493c2c1ae8565
Susumu Yamazaki <zacky1972@gmail.com>
2018-02-26 10:23:15 +0900

    v0.0.3 Urakami

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Zeam.MixProject do
   def project do
     [
       app: :zeam,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.6",
       description: "ZEAM is ZACKY's Elixir Abstract Machine, which is aimed at being BEAM compatible.",
       package: [
